0e5da60fea430ad92337907bf4c484ac2f037341
HamidReazaNikoonia <hamidrezanik00nia@gmail.com>
2023-10-05 13:36:18 +0330

    presist-redux

--- src/store/index.js
+++ src/store/index.js
@@ -9,6 +9,20 @@ import interviewReducer from './features/interviewSlice';
 import uiReducer from './features/ui';
 
 // ==============================|| REDUX - MAIN STORE ||============================== //
+//MIDDLEWARE
+const localStorageMiddleware = ({ getState }) => {
+    return (next) => (action) => {
+        const result = next(action);
+        localStorage.setItem('applicationState', JSON.stringify(getState()));
+        return result;
+    };
+};
+
+const reHydrateStore = () => {
+    if (localStorage.getItem('applicationState') !== null) {
+        return JSON.parse(localStorage.getItem('applicationState')); // re-hydrate the store
+    }
+};
 
 const store = configureStore({
     reducer: {
@@ -20,13 +34,15 @@ const store = configureStore({
         interviewState: interviewReducer,
         ui: uiReducer
     },
+    preloadedState: reHydrateStore(),
     devTools: process.env.NODE_ENV === 'development',
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({}).concat([
             authApi.middleware,
             userApi.middleware,
             // Add the PostApi middleware to the store
-            interviewApi.middleware
+            interviewApi.middleware,
+            localStorageMiddleware
         ])
 });
 
